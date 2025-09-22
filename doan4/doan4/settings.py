974f81474ea063277e7356ea417879fb62662251
Locwara <lethanhloc2612004@gmail.com>
2025-09-23 00:10:48 +0700

    23/09

--- doan4/doan4/settings.py
+++ doan4/doan4/settings.py
@@ -91,7 +91,17 @@ cloudinary.config(
     api_key='768143393531413', 
     api_secret='kvBPf1aaObw24uYYl_7gw6EZ2Aw'
 )
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+#     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+# }
 
+# cloudinary.config(
+#     cloud_name=os.environ.get('CLOUD_NAME'),
+#     api_key=os.environ.get('CLOUDINARY_API_KEY'),
+#     api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+# )
 # File storage configuration
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
